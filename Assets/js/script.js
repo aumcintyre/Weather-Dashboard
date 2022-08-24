03f9a3a21564fb08d24bd7f37e0013c40d00fd08
aumcintyre <aumcintyre@gmail.com>
2022-08-24 17:09:35 -0500

    added personal api key

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,4 +1,8 @@
 function initWeather() {
+    // personal API key
+    let APIKey = "6d2634cf057189dfbdc49782f75750f9"
+    
+    // gloabal variables to push data to
     let searchEl = $('')
     let cityEl = $('#search-city')
     let nameEl = $('')
