e963151c1bf2d1feacd5a5e09a795ad066e1e808
aumcintyre <aumcintyre@gmail.com>
2022-08-24 17:50:44 -0500

    working with request URL still

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,12 +1,21 @@
 function initWeather() {
     // personal API key
     let APIKey = "6d2634cf057189dfbdc49782f75750f9"
-    
+
     // gloabal variables to push data to
-    let searchEl = $('')
-    let cityEl = $('#search-city')
-    let nameEl = $('')
-    let weatherPic = $('')
-    
-    
+    let searchEl = document.getElementById("searchBtn");
+    let cityEl = $('#search-city').val();
+    // let nameEl = $('')
+    // let weatherPic = $('')
+
+    function currentWeather(cityName) {
+        let requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+
+        fetch(requestURL)
+        .then((response) => response.json())
+        // .then((data) => console.log(data));
+
+    searchEl.addEventListener("click", console.log(data));
 }
+}
+initWeather();
