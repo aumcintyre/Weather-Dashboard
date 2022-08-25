f5e5a478067d48380b7886879d261cfa32cabef6
aumcintyre <aumcintyre@gmail.com>
2022-08-25 17:38:43 -0500

    trying to work in UV Index

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,11 +1,14 @@
 const cityEl = document.querySelector("#search-city");
 const nameEl = document.getElementById("location-name")
+const currentTime = document.getElementById("today-date")
 const searchEl = document.getElementById("searchBtn");
 const currentTemp = document.getElementById("temperature");
 const currentHumidity = document.getElementById("humidity");
 const currentWind = document.getElementById("wind-speed")
 const APIKey = "6d2634cf057189dfbdc49782f75750f9"
+const now = dayjs().toString()
 
+console.log(now);
 
 function currentWeather(search) {
     let requestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&appid=" + APIKey;
@@ -18,9 +21,35 @@ function currentWeather(search) {
             // let weatherData = data
             console.log(data);
             nameEl.innerHTML = data.name;
+            currentTime.innerHTML = now;
             currentTemp.innerHTML = "Temperature: " + data.main.temp;
             // currentWind.innerHTML = "Wind Speed: " + data.main.wind.speed + "MPH"; Wind is undefined, but speed doesn't work
             currentHumidity.innerHTML = "Humidity: " + data.main.humidity;
+
+            let lat = data.coord.lat;
+            let lon = data.coord.lon;
+            let UVRequestURL = "https://api.openweathermap.org/data/2.5/uvi/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey + "&cnt=1";
+
+
+            fetch(UVRequestURL)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    let UVIndex = document.createElement("span")
+
+                    if (response.data[0].value < 4 ) {
+                        UVIndex.setAttribute("class", "badge badge-success");
+                    }
+                    else if (response.data[0].value < 8) {
+                        UVIndex.setAttribute("class", "badge badge-warning");
+                    }
+                    else {
+                        UVIndex.setAttribute("class", "badge badge-danger");
+                    }
+                })
+
         });
 
 }
